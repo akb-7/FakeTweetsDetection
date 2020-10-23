599d7e6c2bbd007c89931606ecab8a44944e3170
aakash-cse <aakashbabu.2000@gmail.com>
2020-10-23 08:22:38 +0530

    Ready for deployment😎😎

--- Realorfake.py
+++ Realorfake.py
@@ -4,33 +4,27 @@ Created on Sun May  3 21:30:34 2020
 
 @author: Aakash Babu
 """
-# to remove the warning in my code
+
 from warnings import simplefilter
 simplefilter(action='ignore', category=FutureWarning)
 
-import os
-os.chdir("D:\\Studies\\Machine Learning\\Real or Not NLP with Disaster Tweets")
 import pandas as pd
-#import matplotlib.pyplot as plt
 import numpy as np
-#import itertools
+import pickle
+
+
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-train_data = pd.read_csv("D:\\Studies\\Machine Learning\\Real or Not NLP with Disaster Tweets\\data\\train.csv")
+train = pd.read_csv("data/train.csv")
 
-test_data = pd.read_csv("D:\\Studies\\Machine Learning\\Real or Not NLP with Disaster Tweets\\data\\test.csv")
+X = train.iloc[:,3]
+y = train.iloc[:,4]
 
-check=pd.read_csv("D:\\Studies\\Machine Learning\\Real or Not NLP with Disaster Tweets\\data\\sample_submission.csv")
-
-
-x_train = train_data.iloc[:,3] 
-y_train = train_data["target"]
-x_test  = test_data.iloc[:,3] 
-y_test = check["target"]
-
+x_train, x_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
 
 
 #DataFlair - Initialize a TfidfVectorizer
@@ -41,7 +35,7 @@ tfidf_train=tfidf_vectorizer.fit_transform(x_train)
 tfidf_test=tfidf_vectorizer.transform(x_test)
 
 #DataFlair - Initialize a PassiveAggressiveClassifier
-pac=PassiveAggressiveClassifier(max_iter=50)
+pac=PassiveAggressiveClassifier()
 pac.fit(tfidf_train,y_train)
 
 #DataFlair - Predict on the test set and calculate accuracy
@@ -50,7 +44,7 @@ score=accuracy_score(y_test,y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
 
 #DataFlair - Build confusion matrix
-confusion_matrix(y_test,y_pred, labels=[1,0])
+print(confusion_matrix(y_test,y_pred, labels=[1,0]))
 
 
 from sklearn.naive_bayes import MultinomialNB
@@ -62,4 +56,12 @@ y_pred1=nb.predict(tfidf_test.toarray())
 score1=accuracy_score(y_test,y_pred1)
 print(f'Accuracy: {round(score1*100,2)}%')
 
-print(y_pred)
+print(confusion_matrix(y_test,y_pred1, labels=[1,0]))
+
+# saving vectorizer
+with open('tfid.pickle','wb') as f:
+    pickle.dump(tfidf_vectorizer,f)
+
+# saving model
+with open('model-faketweet.pickle','wb') as f:
+    pickle.dump(nb,f)
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, url_for, Markup, jsonify
 import pickle
 
 app = Flask(__name__)
-pickle_in = open('model_fakenews.pickle','rb')
+pickle_in = open('model-faketweet.pickle','rb')
 pac = pickle.load(pickle_in)
 tfid = open('tfid.pickle','rb')
 tfidf_vectorizer = pickle.load(tfid)
@@ -19,7 +19,11 @@ def newscheck():
 	tfidf_test = tfidf_vectorizer.transform(input_data)
 	# predicting the input
 	y_pred = pac.predict(tfidf_test)
-	return jsonify(result = y_pred[0])
+	if y_pred==1:
+		y_pred = "REAL"
+	else:
+		y_pred = "FAKE"
+	return jsonify(result = y_pred)
 
 
 if __name__=='__main__':
